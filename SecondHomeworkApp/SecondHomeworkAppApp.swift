d182a7bbd06ff52e541b676b2d810d7c04cd049e
anton <otlichnonick@yandex.ru>
2022-07-05 22:09:43 +0300

    Initial Commit

---
+++ SecondHomeworkApp/SecondHomeworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondHomeworkAppApp.swift
+//  SecondHomeworkApp
+//
+//  Created by Anton Agafonov on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SecondHomeworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
